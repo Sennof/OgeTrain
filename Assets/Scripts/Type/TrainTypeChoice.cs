6e1e7ef44a54571a35c81dc56defefa784a81748
sennof <arseniynoskin@gmail.com>
2024-12-14 20:04:04 +0300

    game's type choice screen

---
+++ Assets/Scripts/Type/TrainTypeChoice.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrainTypeChoice : MonoBehaviour
+{
+    [SerializeField] private PageSetter PageSetter;
+
+    [SerializeField] private Animator _typeCardsAnim;
+
+    [SerializeField] private List<GameObject> _cards = new();
+
+    private int _curCardIndex = 0;
+
+    public void NextCard()
+    {
+        if(_curCardIndex < _cards.Count)
+        {
+            _curCardIndex++;
+            _typeCardsAnim.Play($"from{_curCardIndex-1}next");
+        }
+    }
+
+    public void PrevCard()
+    {
+        if(_curCardIndex > 0)
+        {
+            _curCardIndex--;
+            _typeCardsAnim.Play($"from{_curCardIndex+1}prev");
+        }
+    }
+
+    public void Play()
+    {
+        PageSetter.ToPage(_curCardIndex+2);
+    }
+}
