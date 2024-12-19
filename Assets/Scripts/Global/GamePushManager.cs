ba1461b103a5e3682f00077a4db6d2f08e3caef3
sennof <arseniynoskin@gmail.com>
2024-12-19 06:36:07 +0300

    Gamepush fix | Build5

--- Assets/Scripts/Global/GamePushManager.cs
+++ Assets/Scripts/Global/GamePushManager.cs
@@ -6,25 +6,14 @@ using GamePush;
 public class GamePushManager : MonoBehaviour
 {
     public static GamePushManager Instancel { get; private set; }
-    private Coroutine _addCor;
 
     private async void Awake()
     {
         await GP_Init.Ready;
-        _addCor = StartCoroutine(TimedAddShowing());
     }
 
     public void ShowAdd()
     {
         GP_Ads.ShowFullscreen();
     }
-
-    private IEnumerator TimedAddShowing()
-    {
-        while (true)
-        {
-            yield return new WaitForSeconds(100);
-            ShowAdd();
-        }
-    } 
 }
